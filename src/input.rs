02a5af2473d13139bbe70f2a0c8719b5721341dc
rini <rini@rinici.de>
2024-07-22 18:45:37 -0300

    non-thread_local input

--- src/input.rs
+++ src/input.rs
@@ -1,44 +1,40 @@
-use std::cell::RefCell;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 
+use parking_lot::Mutex;
 
-thread_local! {
-    static INPUT_STATE: RefCell<InputState> = RefCell::default();
-}
 mod keycode;
 pub use keycode::KeyCode;
 
-#[derive(Default)]
 struct InputState {
-    pressed: HashSet<KeyCode>,
-    just_pressed: HashSet<KeyCode>,
+    pressed: BTreeSet<KeyCode>,
+    just_pressed: BTreeSet<KeyCode>,
 }
+
+static INPUT_STATE: Mutex<InputState> = Mutex::new(InputState {
+    pressed: BTreeSet::new(),
+    just_pressed: BTreeSet::new(),
+});
 
 #[must_use]
 pub fn is_key_pressed(key: KeyCode) -> bool {
-    INPUT_STATE.with_borrow(|input| input.just_pressed.contains(&key))
+    INPUT_STATE.lock().just_pressed.contains(&key)
 }
 
 #[must_use]
 pub fn is_key_down(key: KeyCode) -> bool {
-    INPUT_STATE.with_borrow(|input| input.pressed.contains(&key))
+    INPUT_STATE.lock().pressed.contains(&key)
 }
 
 pub fn press_key(key: KeyCode) {
-    INPUT_STATE.with_borrow_mut(|input| {
-        input.pressed.insert(key);
-        input.just_pressed.insert(key);
-    });
+    let mut input = INPUT_STATE.lock();
+    input.pressed.insert(key);
+    input.just_pressed.insert(key);
 }
 
 pub fn release_key(key: KeyCode) {
-    INPUT_STATE.with_borrow_mut(|input| {
-        input.pressed.remove(&key);
-    })
+    INPUT_STATE.lock().pressed.remove(&key);
 }
 
 pub fn clear() {
-    INPUT_STATE.with_borrow_mut(|input| {
-        input.just_pressed.clear();
-    })
+    INPUT_STATE.lock().just_pressed.clear();
 }
